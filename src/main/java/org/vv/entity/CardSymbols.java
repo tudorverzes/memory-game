3dd5ddd9180365bc0b5646e95b2038d57616be4c
Tudor-Andrei Verzes <verzestudor102@gmail.com>
2025-11-18 18:24:54 +0100

    fix: unicode card symbols

--- src/main/java/org/vv/entity/CardSymbols.java
+++ src/main/java/org/vv/entity/CardSymbols.java
@@ -8,8 +8,8 @@ public class CardSymbols {
 
 
     private static final List<String> SYMBOLS = Arrays.asList(
-            "♥", "♦", "♣", "♠",
-            "★", "♫", "☼", "☺",
+            "1", "2", "3", "4",
+            "5", "6", "7", "8",
             "@", "#", "$", "%",
             "&", "+", "=", "!",
             "A", "B", "C", "D",
